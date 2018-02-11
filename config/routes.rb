be5be6798aa7c576660c49832a7bf0c0ba203238
Hannah Carnes <hecarnes@gmail.com>
2018-02-11 14:36:15 -0500

    require auth for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :questions
+  authenticated do
+    resources :questions
+  end
+  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#welcome", as: :welcome
