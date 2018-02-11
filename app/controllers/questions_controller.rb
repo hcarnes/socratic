29b95adf2f9268b0bc3d2705a306bf4debb8fdaf
Hannah Carnes <hecarnes@gmail.com>
2018-02-11 15:07:03 -0500

    create question and validate

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,20 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def create
+    @question = current_user.questions.build(permitted_question_params)
+
+    if @question.save
+      redirect_to question_path(@question), notice: "Your question has been submitted."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def permitted_question_params
+    params.require(:question).permit(:content)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :user
+  validates_presence_of :content, message: "Empty questions are not virtuous"
 end
