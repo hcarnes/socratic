9c132d3fee7a01a9e6e7dc617d2e5853fd44d63d
Hannah Carnes <hecarnes@gmail.com>
2018-02-11 14:20:37 -0500

    generate questions resource & add styling to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :questions
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#welcome", as: :welcome
