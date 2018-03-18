6d4a8ccaeb8cbadcfdd9d57d9af9b135925544bb
Hannah Carnes <hecarnes@gmail.com>
2018-03-18 18:02:10 -0400

    add popover to name link

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'omniauth-google-oauth2'
 gem 'bootstrap', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'pg', '~> 0.18'
+gem 'active_model_serializers'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(() => {
+  $(".user-info-popover").popover({
+    trigger: 'hover',
+    placement: 'top',
+    content: () => {
+      return "hello world"
+    }
+  })
+
+})
