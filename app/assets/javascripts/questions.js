0ab54118cffa00dce53e6d1e340c8e75eba0373c
Hannah Carnes <hecarnes@gmail.com>
2018-03-18 19:11:01 -0400

    show user profile image on popover

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -3,10 +3,28 @@
 $(document).ready(() => {
   $(".user-info-popover").popover({
     trigger: 'hover',
-    placement: 'top',
-    content: function () {
-      debugger
-    }
+    placement: 'right',
+    content: "Loading...",
+    html: true
   })
 
+  $(".user-info-popover").on('shown.bs.popover', async function () {
+    const po = $(this).data()['bs.popover']
+    po.config.content = await renderUserProfile.call(this)
+    po.setContent()
+  })
+
+  async function renderUserProfile() {
+    const userId = this.dataset.userId
+    const profileResponse = await fetch(`/users/${userId}`, {
+      headers: {
+        'Accept': 'application/json'
+      },
+      credentials: 'same-origin'
+    })
+    const profile = await profileResponse.json()
+
+
+    return `<img src=${profile.image_url} width="100">`
+  }
 })
